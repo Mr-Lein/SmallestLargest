0e983283b6259e358b9d96485cb666bf8eb3a322
Mr-Lein <151097135+Mr-Lein@users.noreply.github.com>
2024-08-21 16:10:37 +0000

    starter code

---
+++ SmallestLargest.java
@@ -0,0 +1,12 @@
+//put your name here
+public class SmallestLargest{
+    public static void main(String[] args){
+
+    }
+    public static int smallestValue(){
+
+    }
+    public static int largestValue(){
+        
+    }
+}
